14599ee846d9b1dacb6a58a361fd310596e73b43
Ryan Dooley <Rdoolz51@aol.com>
2023-06-12 04:06:03 -0400

    deleted Unit Tests that i never got to

--- src/main/java/com/revature/utils/DataEncryptionUtil.java
+++ src/main/java/com/revature/utils/DataEncryptionUtil.java
@@ -1,6 +1,13 @@
 package com.revature.utils;
 
 public class DataEncryptionUtil {
+    String debitCardNum;
+
+    public String encryptData(String debitCardNum) {
+
+
+        return "";
+    }
 
 
 }
